d689bbda8837dc97485fe87135bd1f0aad99679c
kirillstrizenko <krealstrzh@icloud.com>
2022-09-10 15:12:18 +0300

    HW 2_4

---
+++ src/main/java/pro/sky/java/course2/calculator/service/CalculatorImpl.java
@@ -0,0 +1,19 @@
+package pro.sky.java.course2.calculator.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CalculatorImpl implements CalculatorInterface{
+
+    public int additionResult(int num1, int num2) {
+        return num1 + num2;
+    }
+
+    public int subtractionResult(int num1, int num2) {
+        return num1 - num2;
+    }
+
+    public int multiplicationResult(int num1, int num2) {
+        return num1 * num2;
+    }
+}
---
+++ src/main/java/pro/sky/java/course2/calculator/service/CalculatorInterface.java
@@ -0,0 +1,10 @@
+package pro.sky.java.course2.calculator.service;
+
+public interface CalculatorInterface {
+
+    int additionResult(int num1, int num2);
+
+    int subtractionResult(int num1, int num2);
+
+    int multiplicationResult(int num1, int num2);
+}
